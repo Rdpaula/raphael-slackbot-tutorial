4dafde563cbf2348c81d6906f527f015ab035fdb
Raphael Fonseca <raphael.fonseca@ccc.ufcg.edu.br>
2023-09-11 18:30:44 -0300

    feat: add clock counter

--- scheduled.py
+++ scheduled.py
@@ -4,6 +4,7 @@ import time
 import logging
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -13,7 +14,7 @@ def sendMessage(slack_client, msg):
   # check if the request was a success
   try:
     slack_client.chat_postMessage(
-      channel='#test',
+      channel='#creating-bots',
       text=msg
     )#.get()
   except SlackApiError as e:
@@ -21,12 +22,13 @@ def sendMessage(slack_client, msg):
     logging.error(e.response)
 
 if __name__ == "__main__":
+  load_dotenv()
   SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
   slack_client = WebClient(SLACK_BOT_TOKEN)
   logging.debug("authorized slack client")
 
   # # For testing
-  msg = "Good Morning!"
+  msg = "Aprendendo a codar!"
   schedule.every(60).seconds.do(lambda: sendMessage(slack_client, msg))
 
   # schedule.every().monday.at("13:15").do(lambda: sendMessage(slack_client, msg))
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import json
 import logging
+from dotenv import load_dotenv
+from datetime import datetime as dt
 
 from flask import Flask, request, make_response, Response
 
@@ -13,13 +15,7 @@ from slashCommand import Slash
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
-@app.route("/slack/test", methods=["POST"])
-def command():
-  if not verifier.is_valid_request(request.get_data(), request.headers):
-    return make_response("invalid request", 403)
-  info = request.form
-
-  # # send user a response via DM
+  # send user a response via DM
   # im_id = slack_client.im_open(user=info["user_id"])["channel"]["id"]
   # ownerMsg = slack_client.chat_postMessage(
   #   channel=im_id,
@@ -32,9 +28,15 @@ def command():
   #   text=commander.getMessage()
   # )
 
+@app.route("/slack/test", methods=["POST"])
+def command():
+  if not verifier.is_valid_request(request.get_data(), request.headers):
+    return make_response("invalid request", 403)
+  info = request.form
+
   try:
     response = slack_client.chat_postMessage(
-      channel='#{}'.format(info["channel_name"]), 
+      channel='#creating-bots'.format(info["channel_name"]), 
       text=commander.getMessage()
     )#.get()
   except SlackApiError as e:
@@ -44,13 +46,63 @@ def command():
 
   return make_response("", response.status_code)
 
+@app.route("/slack/start", methods=["POST"])
+def start():
+  if not verifier.is_valid_request(request.get_data(), request.headers):
+    return make_response("invalid request", 403)
+  info = request.form
+
+  try:
+    global before
+    before = dt.now()
+    print(before.strftime("%d/%m/%Y %H:%M:%S"))
+
+    response = slack_client.chat_postMessage(
+      channel='#creating-bots'.format(info["channel_name"]), 
+      text="starting clock"
+    )#.get()
+
+  except SlackApiError as e:
+    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+    logging.error(e.response)
+    return make_response("", e.response.status_code)
+
+  return make_response("", response.status_code)
+
+@app.route("/slack/stop", methods=["POST"])
+def stop():
+  if not verifier.is_valid_request(request.get_data(), request.headers):
+    return make_response("invalid request", 403)
+  info = request.form
+
+  try:
+    global before
+    
+    duration = dt.now() - before
+    duration = duration.total_seconds()
+    hours, remainder = divmod(duration, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    response = slack_client.chat_postMessage(
+      channel='#creating-bots'.format(info["channel_name"]), 
+      text='{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
+    )#.get()
+    
+    
+  except SlackApiError as e:
+    logging.error('Request to Slack API Failed: {}.'.format(e.response.status_code))
+    logging.error(e.response)
+    return make_response("", e.response.status_code)
+
+  return make_response("", response.status_code)
+
 # Start the Flask server
 if __name__ == "__main__":
+  load_dotenv()
   SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
   SLACK_SIGNATURE = os.environ['SLACK_SIGNATURE']
   slack_client = WebClient(SLACK_BOT_TOKEN)
   verifier = SignatureVerifier(SLACK_SIGNATURE)
 
   commander = Slash("Hey there! It works.")
-
+  before = dt.now()
   app.run()
